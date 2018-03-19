e7765ec5b941bdaf75a046cfa1b103998c2e02e0
qinmingyuan <mingyuan0715@foxmail.com>
2018-03-19 09:50:38 +0800

    add anlyze

--- lib/the_sync/active_record.rb
+++ lib/the_sync/active_record.rb
@@ -6,8 +6,9 @@ module TheSync::ActiveRecord
   attr_reader :adapter
   # source
   # source_client
-  # source_table
+  # dest_table
   def acts_as_sync(options = {})
+    options[:dest_table] ||= self.table_name
 
     _mappings = options[:mapping].to_h
 
@@ -18,9 +19,9 @@ module TheSync::ActiveRecord
     end
 
     # 'source.table_name'
-    @view_name = options[:source].to_s + '_' + self.table_name
+    @view_name = options[:dest].to_s + '_' + self.table_name
 
-    @source_table = options[:source_table]
+    @dest_table = options[:dest_table]
     @full_mappings = _filter_columns.map { |column_name|
       next if column_name == primary_key
       if _mappings.key?(column_name)
@@ -29,11 +30,12 @@ module TheSync::ActiveRecord
         [column_name, column_name]
       end
     }.compact
-    @source_columns = @full_mappings.map { |col| col[1] } + [primary_key]
+    @dest_pk = _mappings.key?(self.primary_key) ? _mappings[self.primary_key] : primary_key
 
-    @adapter = TheSync::Adapter.adapter(options[:source])
+    @my_columns = @full_mappings.map { |col| col[0] } + [primary_key]
+    @dest_columns = @full_mappings.map { |col| col[1] } + [@dest_pk]
 
-    @source_pk = 'id'
+    @adapter = TheSync::Adapter.adapter(options[:dest])
 
     extend TheSync::Table
     extend TheSync::Analyze
@@ -46,10 +48,10 @@ module TheSync::ActiveRecord
 
   def create_temp_table
     sql = "CREATE TABLE #{@view_name} (\n"
-    sql << sql_table(only: @source_columns)
+    sql << sql_table(only: @dest_columns)
     sql << ")"
     sql << "ENGINE=FEDERATED\n"
-    sql << "CONNECTION='#{adapter.connection}/#{@source_table}';"
+    sql << "CONNECTION='#{adapter.connection}/#{@dest_table}';"
 
     connection.execute(sql)
   end
@@ -57,10 +59,10 @@ module TheSync::ActiveRecord
   def select_view(start: 0, finish: start + 1000)
     sql = <<~HEREDOC
       CREATE TEMPORARY TABLE #{@view_name} \
-      SELECT #{@source_columns.join(',')} \
-      FROM #{@source_table} \
-      WHERE #{@source_pk} >= #{start} AND #{@source_pk} <= #{finish}
-      ORDER BY #{@source_pk} ASC
+      SELECT #{@dest_columns.join(',')} \
+      FROM #{@dest_table} \
+      WHERE #{@dest_pk} >= #{start} AND #{@dest_pk} <= #{finish}
+      ORDER BY #{@dest_pk} ASC
     HEREDOC
 
     @adapter.client.query(sql)
--- lib/the_sync/analyze.rb
+++ lib/the_sync/analyze.rb
@@ -1,46 +1,43 @@
 module TheSync::Analyze
 
   def analyze_diffs
-    query = analyze_table.join(dest_table).on(my_table[primary_key].eq(dest_table[@source_pk]))
+    query = analyze_table.join(dest_arel_table).on(my_arel_table[primary_key].eq(dest_arel_table[@dest_pk]))
     query.where(analyze_conditions)
 
     results = connection.execute(query.to_sql)
   end
-
 
   def analyze_inserts
-    query = analyze_table.join(dest_table, Arel::Nodes::OuterJoin).on(my_table[primary_key].eq(dest_table[@source_pk]))
-    query.where(analyze_conditions)
+    query = analyze_table.join(dest_arel_table, Arel::Nodes::OuterJoin).on(my_arel_table[primary_key].eq(dest_arel_table[@dest_pk]))
 
     results = connection.execute(query.to_sql)
   end
 
-
   def analyze_deletes
-    query = analyze_table.join(dest_table, Arel::Nodes::RightOuterJoin).on(my_table[primary_key].eq(dest_table[@source_pk]))
-    query.where(analyze_conditions)
+    query = analyze_table.join(dest_arel_table, Arel::Nodes::RightOuterJoin).on(my_arel_table[primary_key].eq(dest_arel_table[@dest_pk]))
 
     results = connection.execute(query.to_sql)
   end
 
   def analyze_table
-    attrs = @source_columns.map { |col| my_table[col] }
-    my_table.project(*attrs)
+    attrs = @my_columns.map { |col| my_arel_table[col] }
+    attrs += @dest_columns.map { |col| dest_arel_table[col] }
+    my_arel_table.project(*attrs)
   end
 
-  def my_table
-    @my_table ||= Arel::Table.new(self.table_name)
+  def my_arel_table
+    @my_arel_table ||= Arel::Table.new(self.table_name)
   end
 
-  def dest_table
-    @dest_table ||= Arel::Table.new(@view_name)
+  def dest_arel_table
+    @dest_arel_table ||= Arel::Table.new(@view_name)
   end
 
   def analyze_conditions
     first_mapping = @full_mappings[0]
-    condition = my_table[first_mapping[0]].not_eq(dest_table[first_mapping[1]])
+    condition = my_arel_table[first_mapping[0]].not_eq(dest_arel_table[first_mapping[1]])
     @full_mappings[1..-1].each do |mapping|
-      condition = condition.or( my_table[mapping[0]].not_eq(dest_table[mapping[1]]) )
+      condition = condition.or( my_arel_table[mapping[0]].not_eq(dest_arel_table[mapping[1]]) )
     end
     condition
   end
