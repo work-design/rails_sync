d8169e28b892b86c58a209918a463060dbced6b5
qinmingyuan <mingyuan0715@foxmail.com>
2018-04-02 11:54:39 +0800

    fix job error

--- app/jobs/audit_apply_job.rb
+++ app/jobs/audit_apply_job.rb
@@ -1,8 +1,8 @@
 class AuditApplyJob < ActiveJob::Base
   queue_as :default
 
-  def perform(synchro_type, action: ['update', 'insert', 'delete'])
-    SyncAudit.synchro_apply(synchro_type, action: action)
+  def perform(synchro_type, operation: ['update', 'insert', 'delete'])
+    SyncAudit.synchro_apply(synchro_type, operation: operation)
   end
 
 end
