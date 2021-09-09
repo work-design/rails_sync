a4e53f3eda82836c770bbd7b8e77d764f4a7b093
qinmingyuan <mingyuan0715@foxmail.com>
2021-09-09 10:25:16 +0800

    add log dir

--- lib/rails_sync/engine.rb
+++ lib/rails_sync/engine.rb
@@ -1,7 +1,9 @@
 module RailsSync
   class Engine < ::Rails::Engine
 
-    config.eager_load_paths += Dir["#{config.root}/app/models/mysql"]
+    config.eager_load_paths += Dir[
+      "#{config.root}/app/models/mysql"
+    ]
 
   end
 end
