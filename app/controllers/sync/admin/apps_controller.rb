95c61b88eaf34e12de7af6423627de416febb81c
qinmingyuan <mingyuan0715@foxmail.com>
2024-12-16 10:25:50 +0800

    d

--- app/controllers/sync/admin/apps_controller.rb
+++ app/controllers/sync/admin/apps_controller.rb
@@ -6,19 +6,12 @@ module Sync
       q_params = {}
       q_params.merge! params.permit(:id, :type, :appid)
 
-      @apps = App.includes(:organ).default_where(q_params).order(id: :desc).page(params[:page])
+      @apps = App.default_where(q_params).order(id: :desc).page(params[:page])
     end
 
     def create
       @app = App.find_or_initialize_by(appid: app_params[:appid])
-      if @app.organ
-        @app.errors.add :base, '该账号已在其他组织添加，请联系客服'
-      end
       @app.assign_attributes app_params
-
-      unless @app.save
-        render :new, locals: { model: @app }, status: :unprocessable_entity
-      end
     end
 
     private
